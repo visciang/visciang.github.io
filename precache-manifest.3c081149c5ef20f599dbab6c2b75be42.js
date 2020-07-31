92ec5af6f48751029a909771d0dda6322226c2f5
Giovanni Visciano <giovanni.visciano@statpro.com>
2020-07-31 09:53:54 +0200

    init

---
+++ precache-manifest.3c081149c5ef20f599dbab6c2b75be42.js
@@ -0,0 +1,266 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "04435f64d512c4559c4f4d3bc284dc2f",
+    "url": "https://visciang.github.io/index.html"
+  },
+  {
+    "revision": "1f662592a9922a26c39b",
+    "url": "https://visciang.github.io/static/css/4.8921c4c4.chunk.css"
+  },
+  {
+    "revision": "1e6a41a073e17e8ff1d6",
+    "url": "https://visciang.github.io/static/css/main.4b400197.chunk.css"
+  },
+  {
+    "revision": "73a2f698ae05b3382001",
+    "url": "https://visciang.github.io/static/js/0.b97f91b6.chunk.js"
+  },
+  {
+    "revision": "c6f887b076e5a33bc872",
+    "url": "https://visciang.github.io/static/js/10.816ccbe9.chunk.js"
+  },
+  {
+    "revision": "a63bb0464137d4734c17",
+    "url": "https://visciang.github.io/static/js/11.59d33fa5.chunk.js"
+  },
+  {
+    "revision": "06da1742786d98ded9ba",
+    "url": "https://visciang.github.io/static/js/12.9533899b.chunk.js"
+  },
+  {
+    "revision": "58bdf68bb59c69af4997",
+    "url": "https://visciang.github.io/static/js/13.ebb1d76a.chunk.js"
+  },
+  {
+    "revision": "11aa44f6d19a51b7d4f6",
+    "url": "https://visciang.github.io/static/js/14.c5d2cd33.chunk.js"
+  },
+  {
+    "revision": "90b6b156a5f62810eddc9189ae0e43d4",
+    "url": "https://visciang.github.io/static/js/14.c5d2cd33.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dc7cbd244764f3f97221",
+    "url": "https://visciang.github.io/static/js/15.0b9021a5.chunk.js"
+  },
+  {
+    "revision": "00a053b010373447826b",
+    "url": "https://visciang.github.io/static/js/16.620baca2.chunk.js"
+  },
+  {
+    "revision": "90915857e892026f890a",
+    "url": "https://visciang.github.io/static/js/17.8f16c01c.chunk.js"
+  },
+  {
+    "revision": "684a856333a4f4ffe2ea",
+    "url": "https://visciang.github.io/static/js/18.c8eaed51.chunk.js"
+  },
+  {
+    "revision": "f4874faaa371b66ac515",
+    "url": "https://visciang.github.io/static/js/19.b9c2d39a.chunk.js"
+  },
+  {
+    "revision": "739c17e0573247db7873",
+    "url": "https://visciang.github.io/static/js/20.24c04272.chunk.js"
+  },
+  {
+    "revision": "492605606b70ca53eac0",
+    "url": "https://visciang.github.io/static/js/21.9bc8ec02.chunk.js"
+  },
+  {
+    "revision": "83582dca3fd30fdcd543",
+    "url": "https://visciang.github.io/static/js/22.531a4bcd.chunk.js"
+  },
+  {
+    "revision": "b7908d14059554d7e380",
+    "url": "https://visciang.github.io/static/js/23.b9ad712d.chunk.js"
+  },
+  {
+    "revision": "7184a35565a1fce4887b",
+    "url": "https://visciang.github.io/static/js/24.7b3bc0c5.chunk.js"
+  },
+  {
+    "revision": "bd99f693b996c7361e09",
+    "url": "https://visciang.github.io/static/js/25.ddbccbe7.chunk.js"
+  },
+  {
+    "revision": "7e02620aadce9f288813",
+    "url": "https://visciang.github.io/static/js/26.b87e4f15.chunk.js"
+  },
+  {
+    "revision": "d91d1495ff161a0340ed",
+    "url": "https://visciang.github.io/static/js/27.ee2d4820.chunk.js"
+  },
+  {
+    "revision": "ea96323c26ed069fe60a",
+    "url": "https://visciang.github.io/static/js/28.6d50a193.chunk.js"
+  },
+  {
+    "revision": "1269a7d298870b32568a",
+    "url": "https://visciang.github.io/static/js/29.1cdb91db.chunk.js"
+  },
+  {
+    "revision": "c588af4a00f62da83ebe",
+    "url": "https://visciang.github.io/static/js/30.ff47c039.chunk.js"
+  },
+  {
+    "revision": "8ce65fd41892359031ad",
+    "url": "https://visciang.github.io/static/js/31.c129cbc5.chunk.js"
+  },
+  {
+    "revision": "3244651141b8b9d07626",
+    "url": "https://visciang.github.io/static/js/32.5782e42f.chunk.js"
+  },
+  {
+    "revision": "3bfdadff881830c93fc6",
+    "url": "https://visciang.github.io/static/js/33.cf6235e5.chunk.js"
+  },
+  {
+    "revision": "9abc61381466bdbe4908",
+    "url": "https://visciang.github.io/static/js/34.cfc2b1b4.chunk.js"
+  },
+  {
+    "revision": "0c1b024a2c170fbd11de",
+    "url": "https://visciang.github.io/static/js/35.c4421140.chunk.js"
+  },
+  {
+    "revision": "e41ba57f6a35c9b9b274",
+    "url": "https://visciang.github.io/static/js/36.fb279032.chunk.js"
+  },
+  {
+    "revision": "ecb43e81c5173626c306",
+    "url": "https://visciang.github.io/static/js/37.8eee1600.chunk.js"
+  },
+  {
+    "revision": "6a9bd3138c95bde04d31",
+    "url": "https://visciang.github.io/static/js/38.b1a2c1dd.chunk.js"
+  },
+  {
+    "revision": "dc77ab875a7a5c9fe97c",
+    "url": "https://visciang.github.io/static/js/39.36de16bf.chunk.js"
+  },
+  {
+    "revision": "1f662592a9922a26c39b",
+    "url": "https://visciang.github.io/static/js/4.72a791a8.chunk.js"
+  },
+  {
+    "revision": "c87550333da8df8ab4c80bd1d6e9defb",
+    "url": "https://visciang.github.io/static/js/4.72a791a8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1cbea9f876781391c0a9",
+    "url": "https://visciang.github.io/static/js/40.b5dc7cf8.chunk.js"
+  },
+  {
+    "revision": "67d7af868a09aa8068d6",
+    "url": "https://visciang.github.io/static/js/41.29366309.chunk.js"
+  },
+  {
+    "revision": "13b7ebf06d0f9dfcda53",
+    "url": "https://visciang.github.io/static/js/42.1d63db07.chunk.js"
+  },
+  {
+    "revision": "dd22f068d4942c60f1eb",
+    "url": "https://visciang.github.io/static/js/43.30b21332.chunk.js"
+  },
+  {
+    "revision": "ee165b705c689352b655",
+    "url": "https://visciang.github.io/static/js/44.c343809d.chunk.js"
+  },
+  {
+    "revision": "8b9f66893a7190bdba07",
+    "url": "https://visciang.github.io/static/js/45.488808a8.chunk.js"
+  },
+  {
+    "revision": "aa856e0bd4f686bd6f54",
+    "url": "https://visciang.github.io/static/js/46.5cf4cf57.chunk.js"
+  },
+  {
+    "revision": "42e29b65fe71ac12490f",
+    "url": "https://visciang.github.io/static/js/47.c88c3616.chunk.js"
+  },
+  {
+    "revision": "ad5719a1c06015f04f0f",
+    "url": "https://visciang.github.io/static/js/48.c3e92b61.chunk.js"
+  },
+  {
+    "revision": "60a57ff779b6ad6c2008",
+    "url": "https://visciang.github.io/static/js/49.1e118937.chunk.js"
+  },
+  {
+    "revision": "2c4323275c9a293247ad",
+    "url": "https://visciang.github.io/static/js/5.e67b3b07.chunk.js"
+  },
+  {
+    "revision": "03a167d800f940cce7de",
+    "url": "https://visciang.github.io/static/js/50.99dd4a13.chunk.js"
+  },
+  {
+    "revision": "204fa6abfd3ec808c70b",
+    "url": "https://visciang.github.io/static/js/51.0f7e46d7.chunk.js"
+  },
+  {
+    "revision": "ee18447508a04460ca54",
+    "url": "https://visciang.github.io/static/js/52.ae4030db.chunk.js"
+  },
+  {
+    "revision": "383899e60ed69a9a53414c6836d91ab5",
+    "url": "https://visciang.github.io/static/js/52.ae4030db.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a279cbb39c989cad9f0d",
+    "url": "https://visciang.github.io/static/js/53.f484a880.chunk.js"
+  },
+  {
+    "revision": "a20691f4bb4dad50d1f5",
+    "url": "https://visciang.github.io/static/js/54.35be48fb.chunk.js"
+  },
+  {
+    "revision": "13a35437986363d00931",
+    "url": "https://visciang.github.io/static/js/55.cb7d3e4b.chunk.js"
+  },
+  {
+    "revision": "6acdf7d66732bb180d9f",
+    "url": "https://visciang.github.io/static/js/56.8612a394.chunk.js"
+  },
+  {
+    "revision": "6de9bb01711d02bed5f1",
+    "url": "https://visciang.github.io/static/js/57.ef66ac18.chunk.js"
+  },
+  {
+    "revision": "b3ba0cda1887a78233fc",
+    "url": "https://visciang.github.io/static/js/58.425b7ca4.chunk.js"
+  },
+  {
+    "revision": "383899e60ed69a9a53414c6836d91ab5",
+    "url": "https://visciang.github.io/static/js/58.425b7ca4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9c512554722529422d0b",
+    "url": "https://visciang.github.io/static/js/6.40420218.chunk.js"
+  },
+  {
+    "revision": "44e494db3d9d581a1bf9",
+    "url": "https://visciang.github.io/static/js/7.9788cf6f.chunk.js"
+  },
+  {
+    "revision": "ea2c4419a29e184852d2",
+    "url": "https://visciang.github.io/static/js/8.c3baaa78.chunk.js"
+  },
+  {
+    "revision": "f0e0b0abc3a0a91fa149",
+    "url": "https://visciang.github.io/static/js/9.2f7840fc.chunk.js"
+  },
+  {
+    "revision": "1e6a41a073e17e8ff1d6",
+    "url": "https://visciang.github.io/static/js/main.ef33eb80.chunk.js"
+  },
+  {
+    "revision": "dedb712a07471d6040bb",
+    "url": "https://visciang.github.io/static/js/polyfills-css-shim.91e3f579.chunk.js"
+  },
+  {
+    "revision": "83d9d203dfd0e589bac0",
+    "url": "https://visciang.github.io/static/js/runtime-main.f80c1ff2.js"
+  }
+]);
