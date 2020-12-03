1a5eb41704e48a0117872c495a33f4d2f24f34c9
Giovanni Visciano <giovanni.visciano@athonet.com>
2020-12-03 17:08:39 +0100

    Updates

---
+++ static/js/53.6698f4de.chunk.js
@@ -0,0 +1,2 @@
+(this.webpackJsonpbpc=this.webpackJsonpbpc||[]).push([[53],{138:function(n,t,e){"use strict";e.r(t),e.d(t,"startStatusTap",(function(){return c}));var o=e(16),c=function(){var n=window;n.addEventListener("statusTap",(function(){Object(o.h)((function(){var t=n.innerWidth,e=n.innerHeight,c=document.elementFromPoint(t/2,e/2);if(c){var i=c.closest("ion-content");i&&i.componentOnReady().then((function(){Object(o.f)((function(){return i.scrollToTop(300)}))}))}}))}))}}}]);
+//# sourceMappingURL=53.6698f4de.chunk.js.map
